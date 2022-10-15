805b04c0cb2298934f27c16d49432929a40fadd7
retif <retif@decovar.dev>
2022-10-15 13:09:18 +0200

    [glfw] new port

---
+++ ports/glfw/portfile.cmake
@@ -0,0 +1,32 @@
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL git@github.com:glfw/glfw.git
+    REF 7482de6071d21db77a7236155da44c172a7f6c9e
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DGLFW_BUILD_EXAMPLES=0
+        -DGLFW_BUILD_TESTS=0
+        -DGLFW_BUILD_DOCS=0
+)
+
+vcpkg_cmake_install()
+
+# fixing possible problems with imported targets, such as "Policy CMP0111
+# is not set: An imported target missing its location property fails during generation" and so on
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME "glfw3"
+    CONFIG_PATH "lib/cmake/glfw3"
+)
+
+#vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(
+    INSTALL "${SOURCE_PATH}/LICENSE.md"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+    RENAME copyright
+)
