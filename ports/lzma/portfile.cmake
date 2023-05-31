62f17cce567c9b48974ae7cb3e35cc6e503dea44
retif <retif@decovar.dev>
2023-05-31 16:09:44 +0200

    [lzma] new port

---
+++ ports/lzma/portfile.cmake
@@ -0,0 +1,32 @@
+# does not export symbols for making a DLL
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://7-zip.org/a/lzma2201.7z"
+    FILENAME "lzma2201.7z"
+    SHA512 83e320242a51cb37caefd386a5768e64c6872101fd3c3cf4d3318bf834d27e84d8a92e3252987520a578b19ce3e2cddcbf9e10b909f5263c5cdc0710c2f26797
+)
+
+vcpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+    NO_REMOVE_ONE_LEVEL
+)
+
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in" DESTINATION "${SOURCE_PATH}")
+
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(
+    COPY "${CMAKE_CURRENT_LIST_DIR}/copyright"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+)
