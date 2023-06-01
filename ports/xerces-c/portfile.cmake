cb333aa8a44db298d10a9d8c1c1f61744a661dd7
retif <retif@decovar.dev>
2023-06-01 13:47:02 +0200

    [xerces-c][3.2.4] better transcoder handling and a patch for UWP

--- ports/xerces-c/portfile.cmake
+++ ports/xerces-c/portfile.cmake
@@ -7,28 +7,33 @@ vcpkg_from_git(
         disable-tests-samples-docs-pkg.patch
         remove-dll-export-macro.patch
         unified-cmake-install-path.patch
+        file-manager-uwp.patch # https://gitlab.linphone.org/BC/public/external/xerces-c/-/merge_requests/4
 )
 file(REMOVE "${SOURCE_PATH}/cmake/FindICU.cmake")
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         network network
-        icu CMAKE_REQUIRE_FIND_PACKAGE_ICU
+        transcoder-icu CMAKE_REQUIRE_FIND_PACKAGE_ICU
 )
 
-if("icu" IN_LIST FEATURES)
+if("transcoder-icu" IN_LIST FEATURES)
     vcpkg_list(APPEND FEATURE_OPTIONS "-Dtranscoder=icu")
+elseif("transcoder-iconv" IN_LIST FEATURES) # OR VCPKG_TARGET_IS_UWP OR VCPKG_TARGET_IS_ANDROID OR VCPKG_HOST_IS_OSX
+    vcpkg_list(APPEND FEATURE_OPTIONS "-Dtranscoder=iconv")
+# elseif(VCPKG_HOST_IS_OSX) # OR "transcoder-iconv" IN_LIST FEATURES
+#     # because of a bug in the transcoder selection script, the option
+#     # `macosunicodeconverter` is always selected when building on Mac OS,
+#     # regardless of the target platform, and that breaks cross-compiling.
+#     # As a workaround we force `iconv`, which should at least work for iOS.
+#     # Upstream fix: https://github.com/apache/xerces-c/pull/52
+#     vcpkg_list(APPEND FEATURE_OPTIONS "-Dtranscoder=iconv")
 elseif(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_list(APPEND FEATURE_OPTIONS "-Dtranscoder=windows")
 elseif(VCPKG_TARGET_IS_OSX)
     vcpkg_list(APPEND FEATURE_OPTIONS "-Dtranscoder=macosunicodeconverter")
 else()
     # it chooses gnuiconv or iconv in ./cmake/XercesTranscoderSelection.cmake
-    # so you might need to add iconv to dependencies in vcpkg.json:
-    # {
-    #     "name": "iconv",
-    #     "platform": "!windows & !osx"
-    # }
 endif()
 
 if("xmlch-wchar" IN_LIST FEATURES)
