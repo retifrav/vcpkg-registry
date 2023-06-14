b673bce3f15db3df71271f43982c10e1c2805b12
retif <retif@decovar.dev>
2023-06-14 10:56:55 +0200

    [zstd] version 1.5.5

--- ports/zstd/portfile.cmake
+++ ports/zstd/portfile.cmake
@@ -1,17 +1,27 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:facebook/zstd.git
-    REF e47e674cd09583ff0503f0f6defd6d23d8b718d3
+    REF 63779c798237346c2b245c546c40b72a5a5913fe
     PATCHES
+        no-static-suffix.patch
+        fix-emscripten-and-clang-cl.patch
+        threads-for-ios.patch
         disable-pkgconfig.patch
 )
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" ZSTD_BUILD_STATIC)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ZSTD_BUILD_SHARED)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/build/cmake"
     OPTIONS
+        -DZSTD_BUILD_SHARED=${ZSTD_BUILD_SHARED}
+        -DZSTD_BUILD_STATIC=${ZSTD_BUILD_STATIC}
+        -DZSTD_LEGACY_SUPPORT=1
         -DZSTD_BUILD_PROGRAMS=0
-        -DZSTD_BUILD_SHARED=0
         -DZSTD_BUILD_TESTS=0
+        -DZSTD_BUILD_CONTRIB=0
+        -DZSTD_MULTITHREAD_SUPPORT=1
 )
 
 vcpkg_cmake_install()
@@ -20,8 +30,15 @@ vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(
-    INSTALL "${SOURCE_PATH}/LICENSE"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-    RENAME copyright
+if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    foreach(HEADER IN ITEMS zdict.h zstd.h zstd_errors.h)
+        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/${HEADER}" "defined(ZSTD_DLL_IMPORT) && (ZSTD_DLL_IMPORT==1)" "1" )
+    endforeach()
+endif()
+
+vcpkg_install_copyright(
+    COMMENT "Zstandard is dual-licensed under BSD and GPLv2."
+    FILE_LIST
+       "${SOURCE_PATH}/LICENSE"
+       "${SOURCE_PATH}/COPYING"
 )
