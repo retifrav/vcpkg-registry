d4230bb8de27bb4198632a41884e0e9fe0790a33
retif <retif@decovar.dev>
2023-07-26 13:58:43 +0200

    [decovar-vcpkg-cmake][2022-10-15] conditional version config

--- ports/decovar-vcpkg-cmake/common/Installing.cmake
+++ ports/decovar-vcpkg-cmake/common/Installing.cmake
@@ -15,6 +15,19 @@ install(EXPORT "${PROJECT_NAME}Targets"
     FILE "${PROJECT_NAME}Targets.cmake"
     DESTINATION "share/${PROJECT_NAME}"
 )
+
+set(PROJECT_HAS_VERSION 0)
+if(DEFINED PROJECT_VERSION AND NOT ${PROJECT_VERSION} STREQUAL "")
+    set(PROJECT_HAS_VERSION 1)
+endif()
+if(PROJECT_HAS_VERSION)
+    # generate the version file for the config file
+    write_basic_package_version_file(
+        "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+        COMPATIBILITY AnyNewerVersion
+    )
+endif()
+
 # create config file
 configure_package_config_file(
     "${CMAKE_CURRENT_SOURCE_DIR}/Config.cmake.in"
@@ -24,5 +37,6 @@ configure_package_config_file(
 # install config files
 install(FILES
     "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
+    "$<$<BOOL:${PROJECT_HAS_VERSION}>:${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake>"
     DESTINATION "share/${PROJECT_NAME}"
 )
