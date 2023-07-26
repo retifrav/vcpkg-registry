2b86d55d29833edc08b321fe40af888efb679aed
retif <retif@decovar.dev>
2023-07-26 12:23:05 +0200

    [decovar-vcpkg-cmake][2022-10-15] installing common files

--- ports/decovar-vcpkg-cmake/portfile.cmake
+++ ports/decovar-vcpkg-cmake/portfile.cmake
@@ -1,15 +1,21 @@
 if(VCPKG_CROSSCOMPILING)
     # make FATAL_ERROR in CI when issue #16773 fixed
-    message(WARNING "decovar-vcpkg-cmake is a host-only port; mark it as a host port in your dependencies")
+    message(WARNING "decovar-vcpkg-cmake is a host-only port, mark it as a host port in your ports dependencies")
 endif()
 
+# functions
 file(
     INSTALL
-        "${CMAKE_CURRENT_LIST_DIR}/Installing.cmake"
-        "${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in"
         "${CMAKE_CURRENT_LIST_DIR}/decovar_vcpkg_cmake_ololo.cmake"
         "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+)
+# common files
+file(
+    INSTALL
+        "${CMAKE_CURRENT_LIST_DIR}/common/Installing.cmake"
+        "${CMAKE_CURRENT_LIST_DIR}/common/Config.cmake.in"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/common"
 )
 
 file(
@@ -19,5 +25,4 @@ file(
     RENAME copyright
 )
 
-# what is that
 set(VCPKG_POLICY_CMAKE_HELPER_PORT enabled)
