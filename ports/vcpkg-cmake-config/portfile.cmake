75c9ac50db34ee65c478fe9a8678bc636284dc84
retif <retif@decovar.dev>
2023-07-26 14:36:41 +0200

    [vcpkg-cmake-config][2022-02-06] better error message, common license

--- ports/vcpkg-cmake-config/portfile.cmake
+++ ports/vcpkg-cmake-config/portfile.cmake
@@ -1,12 +1,18 @@
-if(NOT TARGET_TRIPLET STREQUAL _HOST_TRIPLET)
-    # make FATAL_ERROR in CI when issue #16773 fixed
-    message(WARNING "vcpkg-cmake-config is a host-only port; please mark it as a host port in your dependencies.")
+if(VCPKG_CROSSCOMPILING)
+    # should be FATAL_ERROR
+    message(WARNING "${PORT} is a host-only port, mark it as a host dependency in your ports")
 endif()
 
 file(INSTALL
     "${CMAKE_CURRENT_LIST_DIR}/vcpkg_cmake_config_fixup.cmake"
     "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
-    "${CMAKE_CURRENT_LIST_DIR}/copyright"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+)
+
+file(INSTALL
+    "${VCPKG_ROOT_DIR}/LICENSE.txt"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+    RENAME copyright
+)
 
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
