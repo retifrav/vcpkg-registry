257053def0d754e4c37b2261519232fac8635f6d
retif <retif@decovar.dev>
2023-07-26 13:33:15 +0200

    [dearimgui][1.89.3] using common files for installation

--- ports/dearimgui/portfile.cmake
+++ ports/dearimgui/portfile.cmake
@@ -4,9 +4,18 @@ vcpkg_from_git(
     REF 458a1090314a965dd37b02c918d83077a0142ad5
 )
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt"   DESTINATION "${SOURCE_PATH}")
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/Installing.cmake" DESTINATION "${SOURCE_PATH}")
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in"  DESTINATION "${SOURCE_PATH}")
+file(COPY
+    "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt"
+    DESTINATION "${SOURCE_PATH}"
+)
+file(COPY
+    "${CURRENT_HOST_INSTALLED_DIR}/share/decovar-vcpkg-cmake/common/Installing.cmake"
+    DESTINATION "${SOURCE_PATH}"
+)
+file(COPY
+    "${CURRENT_HOST_INSTALLED_DIR}/share/decovar-vcpkg-cmake/common/Config.cmake.in"
+    DESTINATION "${SOURCE_PATH}"
+)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
