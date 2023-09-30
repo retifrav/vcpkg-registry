d44a8d0eaab7d60f0a31b8e75d5cd6a241711eff
retif <retif@decovar.dev>
2023-09-30 12:48:41 +0200

    Toolchain and triplet for building combined/fat/universal iOS binaries

---
+++ toolchains/ios-fat.cmake
@@ -0,0 +1,14 @@
+# for $ENV{VCPKG_ROOT} to work the triplet should contain `set(VCPKG_ENV_PASSTHROUGH_UNTRACKED VCPKG_ROOT)`
+include("$ENV{VCPKG_ROOT}/scripts/toolchains/ios.cmake")
+
+# to make a Mach-O combined/fat/universal binary, one needs to set both architectures
+set(CMAKE_OSX_ARCHITECTURES "arm64;x86_64")
+# and set Xcode attributes to specify which architecture is for actual devices
+set(CMAKE_XCODE_ATTRIBUTE_ARCHS[sdk=iphoneos*] "arm64")
+set(CMAKE_XCODE_ATTRIBUTE_VALID_ARCHS[sdk=iphoneos*] "arm64")
+# and which architecture is for simulator
+set(CMAKE_XCODE_ATTRIBUTE_ARCHS[sdk=iphonesimulator*] "x86_64")
+set(CMAKE_XCODE_ATTRIBUTE_VALID_ARCHS[sdk=iphonesimulator*] "x86_64")
+# and probably also set these flags
+#set(CMAKE_IOS_INSTALL_COMBINED 1)
+#set(CMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH 0)
---
+++ triplets/decovar-arm64-ios-fat.cmake
@@ -0,0 +1,9 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME iOS)
+
+# without this the VCPKG_ROOT environment variable won't be available in the chainloaded toolchain
+set(VCPKG_ENV_PASSTHROUGH_UNTRACKED VCPKG_ROOT)
+# setting Xcode attributes for specifying device/simulator architectures
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE ${CMAKE_CURRENT_LIST_DIR}/../toolchains/ios-fat.cmake)
