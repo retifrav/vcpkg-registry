810bbea670e99744c48ba9e54497b2c7b6bbd160
retif <retif@decovar.dev>
2023-10-30 17:17:31 +0100

    [vcpkg-cmake-get-vars] new port

---
+++ ports/vcpkg-cmake-get-vars/portfile.cmake
@@ -0,0 +1,18 @@
+if(VCPKG_CROSSCOMPILING)
+    # should be FATAL_ERROR
+    message(WARNING "${PORT} is a host-only port, mark it as a host dependency in your ports")
+endif()
+
+file(INSTALL
+    "${CMAKE_CURRENT_LIST_DIR}/vcpkg_cmake_get_vars.cmake"
+    "${CMAKE_CURRENT_LIST_DIR}/cmake_get_vars"
+    "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+file(INSTALL
+    "${VCPKG_ROOT_DIR}/LICENSE.txt"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+    RENAME copyright
+)
+
+set(VCPKG_POLICY_CMAKE_HELPER_PORT enabled)
---
+++ ports/vcpkg-cmake-get-vars/vcpkg-port-config.cmake
@@ -0,0 +1,3 @@
+# shouldn't it be ${CURRENT_PACKAGES_DIR}/share/vcpkg-cmake/vcpkg-port-config.cmake instead?
+include("${CMAKE_CURRENT_LIST_DIR}/../vcpkg-cmake/vcpkg-port-config.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/vcpkg_cmake_get_vars.cmake")
