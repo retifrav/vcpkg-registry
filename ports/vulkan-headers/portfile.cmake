e349f8592961e11b2c755ce3406550d91cbaeb27
retif <retif@decovar.dev>
2024-02-03 15:30:57 +0100

    [vulkan-headers] new port

---
+++ ports/vulkan-headers/portfile.cmake
@@ -0,0 +1,24 @@
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL git@github.com:KhronosGroup/Vulkan-Headers.git
+    REF 217e93c664ec6704ec2d8c36fa116c1a4a1e2d40
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTS=0
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME "VulkanHeaders"
+    CONFIG_PATH "share/cmake/VulkanHeaders"
+)
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug"
+)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
