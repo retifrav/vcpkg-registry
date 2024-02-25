72c27bd9b011d044e6976ecd5fbcc6c0ce9aa72b
retif <retif@decovar.dev>
2024-02-25 20:11:07 +0100

    [glfw] version 3.4.0

--- ports/glfw/portfile.cmake
+++ ports/glfw/portfile.cmake
@@ -1,20 +1,14 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:glfw/glfw.git
-    REF 7482de6071d21db77a7236155da44c172a7f6c9e
+    REF 7b6aead9fb88b3623e3b3725ebb42670cbe4c579
     PATCHES
         disable-pkgconfig.patch
-)
-
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-        vulkan-static GLFW_VULKAN_STATIC
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        ${FEATURE_OPTIONS}
         -DGLFW_BUILD_EXAMPLES=0
         -DGLFW_BUILD_TESTS=0
         -DGLFW_BUILD_DOCS=0
@@ -22,8 +16,6 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-# fixing possible problems with imported targets, such as "Policy CMP0111
-# is not set: An imported target missing its location property fails during generation" and so on
 vcpkg_cmake_config_fixup(
     PACKAGE_NAME "glfw3"
     CONFIG_PATH "lib/cmake/glfw3"
@@ -33,8 +25,4 @@ vcpkg_cmake_config_fixup(
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(
-    INSTALL "${SOURCE_PATH}/LICENSE.md"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-    RENAME copyright
-)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
