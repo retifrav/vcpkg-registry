eba264f96cae9378bd4d43232023d74222434216
retif <retif@decovar.dev>
2024-03-10 15:49:46 +0100

    [draco] version 1.5.7

--- ports/draco/portfile.cmake
+++ ports/draco/portfile.cmake
@@ -1,15 +1,16 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:google/draco.git
-    REF 1af95a20b81624f64c4b19794cb3ca991e6d0a76
+    REF 8786740086a9f4d83f44aa83badfbea4dce7a1b5
     PATCHES
-        installation-tools-pkg.patch
+        installation-and-optional-tools.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        wasm DRACO_WASM
+        gltf-bitstream DRACO_GLTF_BITSTREAM
         tools DRACO_WITH_TOOLS
+        wasm DRACO_WASM
 )
 
 # for some reasons it isn't good enough for them to just use the EMSDK variable
@@ -32,7 +33,6 @@ vcpkg_cmake_configure(
         -DDRACO_JS_GLUE=0
         -DDRACO_UNITY_PLUGIN=0
         -DDRACO_ANIMATION_ENCODING=0
-        -DDRACO_GLTF_BITSTREAM=0
         -DDRACO_MAYA_PLUGIN=0
         -DDRACO_TRANSCODER_SUPPORTED=0
 )
@@ -50,10 +50,8 @@ endif()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH "share/cmake/${PORT}")
 
+vcpkg_fixup_pkgconfig()
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(
-    INSTALL "${SOURCE_PATH}/LICENSE"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-    RENAME copyright
-)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
