7bb4b308dc8c0757c0e189edbcaa955dace147a0
retif <retif@decovar.dev>
2024-05-22 14:56:43 +0200

    [stb] version 2024.2.13

--- ports/stb/portfile.cmake
+++ ports/stb/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:nothings/stb.git
-    REF 5736b15f7ea0ffb08dd38af21067c314d6a3aae9
+    REF ae721c50eaf761660b4f90cc590453cdb0c2acd0
 )
 
 file(COPY
