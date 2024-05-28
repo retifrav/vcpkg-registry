350ac0818ed0051cc36f2f56a5888e17b56a6543
retif <retif@decovar.dev>
2024-05-28 13:25:35 +0200

    [tinygltf] version 2.4.0
    Draco cannot build with the latest version, need to add an older one.

--- ports/tinygltf/portfile.cmake
+++ ports/tinygltf/portfile.cmake
@@ -1,3 +1,6 @@
+# it's a header-only library (in this version)
+set(VCPKG_BUILD_TYPE release)
+
 # does not export symbols for making a DLL
 if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
@@ -6,7 +9,7 @@ endif()
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:syoyo/tinygltf.git
-    REF 4bfc1fc1807e2e2cf3d3111f67d6ebd957514c80
+    REF 91da29972987bb4d715a09d94ecd2cefd3a487d4
     PATCHES
         dependencies-and-installation.patch
 )
@@ -23,17 +26,13 @@ file(COPY
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DTINYGLTF_BUILD_BUILDER_EXAMPLE=0
         -DTINYGLTF_BUILD_GL_EXAMPLES=0
         -DTINYGLTF_BUILD_LOADER_EXAMPLE=0
         -DTINYGLTF_BUILD_VALIDATOR_EXAMPLE=0
-        -DTINYGLTF_HEADER_ONLY=0
 )
 
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup()
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
