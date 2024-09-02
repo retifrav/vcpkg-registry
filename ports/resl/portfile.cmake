d28521a91fd3dbe7460a6c60baa302445a11cf9b
retif <retif@decovar.dev>
2024-09-02 17:04:25 +0200

    [resl] new port

---
+++ ports/resl/portfile.cmake
@@ -0,0 +1,28 @@
+# it's a game, so we don't need a Debug build, and there are no libraries or public headers
+set(VCPKG_BUILD_TYPE release)
+# set(VCPKG_POLICY_ALLOW_EXES_IN_BIN enabled)
+# set(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
+# set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+# set(VCPKG_POLICY_DLLS_IN_STATIC_LIBRARY enabled)
+# set(VCPKG_POLICY_MISMATCHED_NUMBER_OF_BINARIES enabled)
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled) # does all of the above
+
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL git@github.com:konovalov-aleks/reSL.git
+    REF b76ebc5a3572376fda38172b1d7ccd85241b2fc8
+    PATCHES
+        001-non-development-build-and-installation.patch
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_FOR_DEVELOPMENT=0
+)
+
+vcpkg_cmake_install()
+
+# also, since it is not a tool, there is no need to do vcpkg_copy_tools() or anything
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
