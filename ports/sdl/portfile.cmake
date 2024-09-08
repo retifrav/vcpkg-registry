649e1ef1cbe41a88290077eb2adee80d47ecee0e
retif <retif@decovar.dev>
2024-09-08 21:14:40 +0200

    [sdl] version 2.30.7

--- ports/sdl/portfile.cmake
+++ ports/sdl/portfile.cmake
@@ -1,18 +1,25 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:libsdl-org/SDL.git
-    REF fb1497566c5a05e2babdcf45ef0ab5c7cca2c4ae
+    REF 9519b9916cd29a14587af0507292f2bd31dd5752
     PATCHES
-        not-installing-license.patch
+        001-fixed-alsa-condition.patch
+        002-not-installing-license.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" SDL_STATIC)
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" SDL_SHARED)
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" FORCE_STATIC_VCRT)
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        alsa SDL_ALSA
+)
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        ${FEATURE_OPTIONS}
         -DSDL_STATIC=${SDL_STATIC}
         -DSDL_SHARED=${SDL_SHARED}
         -DSDL_FORCE_STATIC_VCRT=${FORCE_STATIC_VCRT}
