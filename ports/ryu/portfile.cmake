7bad94ae60d258227478a3bb2fc2deeb1f1a400a
retif <retif@decovar.dev>
2025-02-14 14:29:40 +0100

    [ryu] new port

---
+++ ports/ryu/portfile.cmake
@@ -0,0 +1,32 @@
+# does not export symbols for making a DLL
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL git@github.com:ulfjack/ryu.git
+    REF 1264a946ba66eab320e927bfd2362e0c8580c42f
+    PATCHES
+        001-installation.patch
+)
+
+file(COPY
+    "${CURRENT_HOST_INSTALLED_DIR}/share/decovar-vcpkg-cmake/common/Config.cmake.in"
+    DESTINATION "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST
+    "${SOURCE_PATH}/LICENSE-Apache2"
+    "${SOURCE_PATH}/LICENSE-Boost"
+)
