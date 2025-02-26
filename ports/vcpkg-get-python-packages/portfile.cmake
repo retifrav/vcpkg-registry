6492b2238133921ec8ff3fa522c6c4bff5d17edf
retif <retif@decovar.dev>
2025-02-26 14:13:41 +0100

    [vcpkg-get-python-packages] version 2024-09-29
    Getting newer pip - previous version fails with newer Python versions,
    as distutils package was removed in Python 3.12, and newer vcpkg
    versions switched to Python 3.12.7.
    
    Some links with more details:
    
    - https://stackoverflow.com/questions/69919970/no-module-named-distutils-util-but-distutils-is-installed/76691103#76691103
    - https://github.com/microsoft/vcpkg/commit/7dcb4ca95bfde3cfca3a9f0a7e933f1d17cc4836#diff-26499ad5d840cada3059f7aaa8363695140b4a6811ed08697b388ab19e16ae97L3
    - https://github.com/microsoft/vcpkg/issues/42021

--- ports/vcpkg-get-python-packages/portfile.cmake
+++ ports/vcpkg-get-python-packages/portfile.cmake
@@ -1,7 +1,4 @@
-if(VCPKG_CROSSCOMPILING)
-    # should be FATAL_ERROR
-    message(WARNING "${PORT} is a host-only port, mark it as a host dependency in your ports")
-endif()
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
 file(INSTALL
     "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
@@ -9,12 +6,7 @@ file(INSTALL
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 )
 
+# not sure if this is needed, nothing fails without it
 #include("${CMAKE_CURRENT_LIST_DIR}/x_vcpkg_get_python_packages.cmake")
 
-file(INSTALL
-    "${VCPKG_ROOT_DIR}/LICENSE.txt"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-    RENAME copyright
-)
-
-set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+vcpkg_install_copyright(FILE_LIST "${CMAKE_CURRENT_LIST_DIR}/copyright")
