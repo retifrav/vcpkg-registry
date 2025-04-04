0c4f9bdad14eb016ad176accb20d310e96914f26
retif <retif@decovar.dev>
2025-04-04 10:49:50 +0200

    [rapidxml][1.13.0] missing required query parameter for SourceForge URLs

--- ports/rapidxml/portfile.cmake
+++ ports/rapidxml/portfile.cmake
@@ -2,7 +2,8 @@
 set(VCPKG_BUILD_TYPE release)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://altushost-swe.dl.sourceforge.net/project/rapidxml/rapidxml/rapidxml%201.13/rapidxml-1.13.zip"
+    URLS
+        "https://altushost-swe.dl.sourceforge.net/project/rapidxml/rapidxml/rapidxml%201.13/rapidxml-1.13.zip?viasf=1"
     FILENAME "rapidxml-1.13.zip"
     SHA512 6c10583e6631ccdb0217d0a5381172cb4c1046226de6ef1acf398d85e81d145228e14c3016aefcd7b70a1db8631505b048d8b4f5d4b0dbf1811d2482eefdd265
 )
