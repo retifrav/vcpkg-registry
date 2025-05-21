da779d984abda968ae158f2fbaa8ca8cbd0b9814
retif <retif@decovar.dev>
2025-05-21 14:21:10 +0200

    [vcpkg-tool-meson] new port

---
+++ ports/vcpkg-tool-meson/install.cmake
@@ -0,0 +1,9 @@
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools/meson")
+
+file(
+    INSTALL
+        "${SOURCE_PATH}/meson.py"
+        "${SOURCE_PATH}/mesonbuild"
+    DESTINATION
+        "${CURRENT_PACKAGES_DIR}/tools/meson"
+)
