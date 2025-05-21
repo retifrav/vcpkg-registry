9df6c6b06bb314032a4fd25609cc846f009619ff
retif <retif@decovar.dev>
2025-05-21 14:19:37 +0200

    [vcpkg-cmake-get-vars][2023-03-02] feng-shui

--- ports/vcpkg-cmake-get-vars/portfile.cmake
+++ ports/vcpkg-cmake-get-vars/portfile.cmake
@@ -3,16 +3,15 @@ if(VCPKG_CROSSCOMPILING)
     message(WARNING "${PORT} is a host-only port, mark it as a host dependency in your ports")
 endif()
 
-file(INSTALL
-    "${CMAKE_CURRENT_LIST_DIR}/vcpkg_cmake_get_vars.cmake"
-    "${CMAKE_CURRENT_LIST_DIR}/cmake_get_vars"
-    "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-
-file(INSTALL
-    "${VCPKG_ROOT_DIR}/LICENSE.txt"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-    RENAME copyright
+file(
+    INSTALL
+        "${CMAKE_CURRENT_LIST_DIR}/cmake_get_vars" # folder
+        "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
+        "${CMAKE_CURRENT_LIST_DIR}/vcpkg_cmake_get_vars.cmake"
+    DESTINATION
+        "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 )
+
+vcpkg_install_copyright(FILE_LIST "${VCPKG_ROOT_DIR}/LICENSE.txt")
 
 set(VCPKG_POLICY_CMAKE_HELPER_PORT enabled)
