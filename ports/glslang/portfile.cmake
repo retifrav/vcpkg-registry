14e302b9ce19c7333e50472b979c219d7703f8b4
retif <retif@decovar.dev>
2025-05-21 14:08:00 +0200

    [glslang] version 15.2.0

--- ports/glslang/portfile.cmake
+++ ports/glslang/portfile.cmake
@@ -1,57 +1,59 @@
-# does not(?) export symbols for making a DLL
-if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-endif()
-
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:KhronosGroup/glslang.git
-    REF d1517d64cfca91f573af1bf7341dc3a5113349c0
+    REF 99ee11b0106b48f06fb9e2a324ba928b8316cc33
     PATCHES
-        install-to-datadir.patch
-        no-threads-on-macos.patch
+        001-threads-and-installation.patch
 )
 
 vcpkg_find_acquire_program(PYTHON3)
 get_filename_component(PYTHON_PATH ${PYTHON3} DIRECTORY)
 vcpkg_add_to_path("${PYTHON_PATH}")
 
-# on iOS executables will require BUNDLE DESTINATION, so that will fail,
-# and on WASM tools are not built(?), so they will fail to copy with vcpkg_copy_tools()
+# this is not in vcpkg_check_features() because on iOS executables will require BUNDLE DESTINATION,
+# so that will fail, and on WASM tools are not built(?), so they will fail to copy with vcpkg_copy_tools()
 set(BUILD_BINARIES 0)
 if("tools" IN_LIST FEATURES AND NOT VCPKG_TARGET_IS_IOS AND NOT VCPKG_TARGET_IS_EMSCRIPTEN)
     set(BUILD_BINARIES 1)
 endif()
 
-set(PLATFORM_OPTIONS "")
-if(VCPKG_TARGET_IS_WINDOWS)
-    list(APPEND PLATFORM_OPTIONS "-DOVERRIDE_MSVCCRT=0")
-endif()
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        opt ENABLE_OPT
+        opt ALLOW_EXTERNAL_SPIRV_TOOLS
+)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DENABLE_GLSLANG_BINARIES=${BUILD_BINARIES}
-        -DSKIP_GLSLANG_INSTALL=0
+        ${FEATURE_OPTIONS}
         -DBUILD_EXTERNAL=0
-        -DBUILD_TESTING=0
-        -DENABLE_CTEST=0
-        ${PLATFORM_OPTIONS}
+        -DENABLE_GLSLANG_BINARIES=${BUILD_BINARIES}
+        -DGLSLANG_ENABLE_INSTALL=1
+        -DGLSLANG_TESTS=0
+        -DGLSLANG_TESTS_DEFAULT=0
 )
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake")
+vcpkg_cmake_config_fixup()
 
 if(NOT BUILD_BINARIES)
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
+    file(REMOVE_RECURSE
+        "${CURRENT_PACKAGES_DIR}/bin"
+    )
 else()
-    vcpkg_copy_tools(TOOL_NAMES glslangValidator spirv-remap AUTO_CLEAN)
+    vcpkg_copy_tools(
+        TOOL_NAMES
+            glslang
+            glslangValidator
+            spirv-remap
+        AUTO_CLEAN
+    )
 endif()
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
-    "${CURRENT_PACKAGES_DIR}/debug/bin"
 )
 
 file(
@@ -59,8 +61,4 @@ file(
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 )
 
-file(
-    INSTALL "${SOURCE_PATH}/LICENSE.txt"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-    RENAME copyright
-)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
