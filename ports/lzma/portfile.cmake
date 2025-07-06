95c3c5d1d0bdc5f2e49c27b8e2fc3d97f94b1261
retif <retif@decovar.dev>
2025-07-06 13:33:59 +0200

    [lzma] version 25.0.0

--- ports/lzma/portfile.cmake
+++ ports/lzma/portfile.cmake
@@ -3,12 +3,12 @@ if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 endif()
 
-set(FILE_TO_DOWNLOAD "lzma2301.7z")
+set(FILE_TO_DOWNLOAD "lzma2500.7z")
 vcpkg_download_distfile(
     ARCHIVE
     URLS "https://7-zip.org/a/${FILE_TO_DOWNLOAD}"
     FILENAME "${FILE_TO_DOWNLOAD}"
-    SHA512 b797912536bd6e9ddb61c7ffcff2579cfccd94cf788535885e52706a0ff6887eba33c978803e0f4d196ff40f6c1976d97ea9ab3457a68744c4465f44deb04cb0
+    SHA512 6dde0dcf1989335524682a79003375c18a7ea74fa55a014ed8a705b749278375b55117c4e764181195e9775cd3e44eb5bed7d4d970e0dfb6d2ae20bdc352baac
 )
 
 vcpkg_extract_source_archive(
@@ -48,7 +48,4 @@ vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(
-    COPY "${CMAKE_CURRENT_LIST_DIR}/copyright"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-)
+vcpkg_install_copyright(FILE_LIST "${CMAKE_CURRENT_LIST_DIR}/copyright")
