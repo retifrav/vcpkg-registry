e9e4ace61cbe798ede3f12a1f0447fc325b49776
retif <retif@decovar.dev>
2025-08-20 15:02:40 +0200

    [vulkan-headers] version 2025.7.4

--- ports/vulkan-headers/portfile.cmake
+++ ports/vulkan-headers/portfile.cmake
@@ -1,13 +1,13 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:KhronosGroup/Vulkan-Headers.git
-    REF 05fe2cc910a68c9ba5dac07db46ef78573acee72
+    REF 2cd90f9d20df57eac214c148f3aed885372ddcfe
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DBUILD_TESTS=0
+        -DVULKAN_HEADERS_ENABLE_TESTS=0
 )
 
 vcpkg_cmake_install()
