71cd55125cd8af1b8f8605a25d0644b7ce3ffcb3
retif <retif@decovar.dev>
2025-08-29 18:39:52 +0200

    [geotiff] version 1.7.4

--- ports/geotiff/portfile.cmake
+++ ports/geotiff/portfile.cmake
@@ -1,9 +1,14 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:OSGeo/libgeotiff.git
-    REF d2c72dba35ac9d1af2201191064ca4cbe7f57f11
+    REF 96024f677642486f97cac43659bef57f4ed0590b
     PATCHES
         001-dependencies-and-installation.patch
+)
+
+# what is this "project" file even for, the library is never used anywhere and it's missing linking to TIFF anyway
+file(REMOVE
+    "${SOURCE_PATH}/libgeotiff/libxtiff/CMakeLists.txt"
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in" DESTINATION "${SOURCE_PATH}/libgeotiff")
@@ -19,6 +24,8 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/libgeotiff"
     OPTIONS
         ${FEATURE_OPTIONS}
+        -DBUILD_DOC=0
+        -DBUILD_MAN=0
 )
 
 vcpkg_cmake_install()
