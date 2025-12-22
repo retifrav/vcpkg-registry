d8eb33b1d97cfeb38425deb4e853bb510756c03b
retif <retif@decovar.dev>
2025-12-22 15:37:58 +0100

    [png] version 1.6.53

--- ports/png/portfile.cmake
+++ ports/png/portfile.cmake
@@ -1,17 +1,25 @@
-vcpkg_download_distfile(ARCHIVE
-    URLS
-        "http://repository.timesys.com/buildsources/l/libpng/libpng-${VERSION}/libpng-${VERSION}.tar.xz"
-        "https://netix.dl.sourceforge.net/project/libpng/libpng16/${VERSION}/libpng-${VERSION}.tar.xz?viasf=1"
-        "https://deac-fra.dl.sourceforge.net/project/libpng/libpng16/${VERSION}/libpng-${VERSION}.tar.xz?viasf=1"
-        "https://files.decovar.dev/public/packages/png/v${VERSION}/src/libpng-${VERSION}.tar.xz"
-    FILENAME "libpng-${VERSION}.tar.xz" # 1.6.43
-    # don't forget that the hash is for 1.6.43 (so actually there is no point in using ${VERSION})
-    SHA512 c95d661fed548708ce7de5d80621a432272bdfe991f0d4db3695036e5fafb8a717b4e4314991bdd3227d7aa07f8c6afb6037c57fa0fe3349334a0b6c58268487
-)
+#vcpkg_download_distfile(ARCHIVE
+#    URLS
+#        "http://repository.timesys.com/buildsources/l/libpng/libpng-${VERSION}/libpng-${VERSION}.tar.xz"
+#        "https://netix.dl.sourceforge.net/project/libpng/libpng16/${VERSION}/libpng-${VERSION}.tar.xz?viasf=1"
+#        "https://deac-fra.dl.sourceforge.net/project/libpng/libpng16/${VERSION}/libpng-${VERSION}.tar.xz?viasf=1"
+#        "https://files.decovar.dev/public/packages/png/v${VERSION}/src/libpng-${VERSION}.tar.xz"
+#    FILENAME "libpng-${VERSION}.tar.xz" # 1.6.43
+#    # don't forget that the hash is for 1.6.43 (so actually there is no point in using ${VERSION})
+#    SHA512 c95d661fed548708ce7de5d80621a432272bdfe991f0d4db3695036e5fafb8a717b4e4314991bdd3227d7aa07f8c6afb6037c57fa0fe3349334a0b6c58268487
+#)
+#
+#vcpkg_extract_source_archive(
+#    SOURCE_PATH
+#    ARCHIVE ${ARCHIVE}
+#    PATCHES
+#        001-single-unsuffixed-target-dependencies-installation.patch
+#)
 
-vcpkg_extract_source_archive(
-    SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL git@github.com:pnggroup/libpng.git
+    REF 4e3f57d50f552841550a36eabbb3fbcecacb7750
     PATCHES
         001-single-unsuffixed-target-dependencies-installation.patch
 )
