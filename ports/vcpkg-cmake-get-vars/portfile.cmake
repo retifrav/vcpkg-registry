82092ee22f0fc84cb2f1173304da68417dee20d0
retif <retif@decovar.dev>
2026-02-11 14:30:42 +0100

    [vcpkg-cmake-get-vars] version 2025-05-29

--- ports/vcpkg-cmake-get-vars/portfile.cmake
+++ ports/vcpkg-cmake-get-vars/portfile.cmake
@@ -1,11 +1,13 @@
 if(VCPKG_CROSSCOMPILING)
     # should be FATAL_ERROR
-    message(WARNING "${PORT} is a host-only port, mark it as a host dependency in your ports")
+    # https://github.com/microsoft/vcpkg/issues/16773
+    message(WARNING "The [${PORT}] port is a host-only port, mark it as a host dependency in your ports")
 endif()
 
 file(
     INSTALL
         "${CMAKE_CURRENT_LIST_DIR}/cmake_get_vars" # folder
+        "${CMAKE_CURRENT_LIST_DIR}/cmake-get-vars.cmake.in"
         "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
         "${CMAKE_CURRENT_LIST_DIR}/vcpkg_cmake_get_vars.cmake"
     DESTINATION
