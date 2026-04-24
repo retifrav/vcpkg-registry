c56eb4e592700e452cf6541fbaef8dd17354d407
retif <retif@decovar.dev>
2026-04-24 09:40:41 +0200

    [vulkan-headers] version 2026.1.23

--- ports/vulkan-headers/portfile.cmake
+++ ports/vulkan-headers/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:KhronosGroup/Vulkan-Headers.git
-    REF 2cd90f9d20df57eac214c148f3aed885372ddcfe
+    REF b5c8f996196ba4aa6d8f97e52b5d3b6e70f7e4e2 # vulkan-sdk-1.4.341.0
 )
 
 vcpkg_cmake_configure(
