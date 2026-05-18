fc083059408110da8d399ca5627e28a99bc6bff0
retif <retif@decovar.dev>
2026-05-18 12:26:00 +0200

    [rapidjson] version 2025.2.5

--- ports/rapidjson/portfile.cmake
+++ ports/rapidjson/portfile.cmake
@@ -4,7 +4,7 @@ set(VCPKG_BUILD_TYPE release)
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:Tencent/rapidjson.git
-    REF 858451e5b7d1c56cf8f6d58f88cf958351837e53
+    REF 24b5e7a8b27f42fa16b96fc70aade9106cf7102f
     PATCHES
         001-installation.patch
 )
@@ -21,9 +21,6 @@ file(COPY
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DRAPIDJSON_BUILD_CXX11=0
-        -DRAPIDJSON_BUILD_CXX17=1
-        -DRAPIDJSON_BUILD_CXX20=0
         -DRAPIDJSON_BUILD_DOC=0
         -DRAPIDJSON_BUILD_EXAMPLES=0
         -DRAPIDJSON_BUILD_TESTS=0
@@ -31,6 +28,7 @@ vcpkg_cmake_configure(
         -DRAPIDJSON_ENABLE_INSTRUMENTATION_OPT=1
         -DRAPIDJSON_HAS_STDSTRING=1
         -DRAPIDJSON_USE_MEMBERSMAP=0
+        -DRAPIDJSON_VERSION="${VERSION}"
 )
 
 vcpkg_cmake_install()
