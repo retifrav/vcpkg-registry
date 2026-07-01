910074fa942f0456d664c1f1bdb123d284cfa5f8
retif <retif@decovar.dev>
2026-07-01 09:29:51 +0200

    [dos2unix][7.5.2] fallback download URLs, setting version in project()

--- ports/dos2unix/portfile.cmake
+++ ports/dos2unix/portfile.cmake
@@ -4,7 +4,9 @@ set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 
 vcpkg_download_distfile(
     ARCHIVE
-    URLS "https://waterlan.home.xs4all.nl/dos2unix/dos2unix-${VERSION}.tar.gz"
+    URLS
+        #"https://altushost-swe.dl.sourceforge.net/project/dos2unix/dos2unix/${VERSION}/dos2unix-${VERSION}.tar.gz?viasf=1"
+        "https://files.decovar.dev/public/packages/dos2unix/v${VERSION}/src/dos2unix-${VERSION}.tar.gz"
     FILENAME "dos2unix-${VERSION}.tar.gz"
     SHA512 d76d799435dd248850f72cc50af2144a51e99f04ea83a1447c4edd828625c83f0afba367da51aa83defced4cbf34f3b75387a0821010f7b212225571036efbb2
 )
