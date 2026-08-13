71224687252ec5e427fd125c55cb2df800807973
retif <retif@decovar.dev>
2026-08-13 19:46:40 +0200

    [zlib] version 1.3.2

--- ports/zlib/portfile.cmake
+++ ports/zlib/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:madler/zlib.git
-    REF 51b7f2abdade71cd9bb0e7a373ef2610ec6f9daf
+    REF da607da739fa6047df13e66a2af6b8bec7c2a498
     PATCHES
-        single-target-and-installation.patch
+        001-single-target-and-installation.patch
 )
 
 file(COPY
@@ -15,34 +15,33 @@ file(COPY
     DESTINATION "${SOURCE_PATH}"
 )
 
-# this file is auto-generated on CMake configure
-file(REMOVE "${SOURCE_PATH}/zconf.h")
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ZLIB_BUILD_SHARED)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" ZLIB_BUILD_STATIC)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DZLIB_BUILD_EXAMPLES=0
+        -DZLIB_BUILD_SHARED=${ZLIB_BUILD_SHARED}
+        -DZLIB_BUILD_STATIC=${ZLIB_BUILD_STATIC}
+        -DZLIB_BUILD_TESTING=0
 )
 
 vcpkg_cmake_install()
 
-# if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-#     if(VCPKG_TARGET_IS_WINDOWS)
-#         vcpkg_replace_string("${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/zlib.pc" "-lz" "-lzlib")
-#     endif()
-#     file(COPY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/zlib.pc" DESTINATION "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
-# endif()
-# if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-#     if(VCPKG_TARGET_IS_WINDOWS)
-#         vcpkg_replace_string("${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/zlib.pc" "-lz" "-lzlibd")
-#     endif()
-#     file(COPY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/zlib.pc" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
-# endif()
-
 vcpkg_cmake_config_fixup()
 
-#vcpkg_fixup_pkgconfig()
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/zlib/zconf.h"
+        "ifdef ZLIB_DLL"
+        "if 0"
+    )
+else()
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/zlib/zconf.h"
+        "ifdef ZLIB_DLL"
+        "if 1"
+    )
+endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-vcpkg_install_copyright(FILE_LIST "${CMAKE_CURRENT_LIST_DIR}/license.txt")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
